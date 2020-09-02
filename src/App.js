410a37b2dee6e350bf1c51300a4ef2b4093bb89d
kharioki <antolisah@gmail.com>
2020-09-03 01:13:06 +0300

    fetching goals and updates from airtable

--- src/App.js
+++ src/App.js
@@ -1,7 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Airtable from 'airtable';
+
+const base = new Airtable({ apiKey: 'keyTLS0P8OB9AiTlW' }).base(
+  'appEDJXKhlklhtwkd'
+);
 
 function App() {
-  return <div>app</div>;
+  const [goals, setGoals] = useState([]);
+  const [updates, setUpdates] = useState([]);
+
+  useEffect(() => {
+    base('goals')
+      .select({ view: 'Grid view' })
+      .eachPage((records, fetchNextPage) => {
+        setGoals(records);
+        fetchNextPage();
+      });
+    base('updates')
+      .select({ view: 'Grid view' })
+      .eachPage((records, fetchNextPage) => {
+        setUpdates(records);
+        fetchNextPage();
+      });
+  }, []);
+
+  return (
+    <>
+      <h1>My Goals</h1>
+    </>
+  );
 }
 
 export default App;
